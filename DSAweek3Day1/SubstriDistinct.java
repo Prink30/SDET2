d111dd29779e418e6141f48e5a1b7caf3d5d841c
Prink30 <112071940+Prink30@users.noreply.github.com>
2023-06-07 17:19:32 +0530

    Add files via upload

---
+++ DSAweek3Day1/SubstriDistinct.java
@@ -0,0 +1,67 @@
+package DSAweek3Day1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class SubstriDistinct {
+
+	@Test
+	public void positive() {
+		String str = "xyzzaw";
+		int k =3;
+		 subStrddDistinct(str, k);
+	}
+	
+	
+	@Test
+	public void negative() {
+		String str = "aaaaaa";
+		int k =3;
+		 subStrddDistinct(str, k);
+	}
+
+	private void subStrddDistinct(String str, int k) {
+		
+		int count=0;
+		Map<Character,Integer> value = new HashMap<Character,Integer>();
+		
+		
+		for(int i=0;i<str.length()-k;i++) {
+			value.put(str.charAt(i), value.getOrDefault(str.charAt(i), 0)+1);
+		}
+		
+	//	System.out.println(Arrays.asList(value));
+		
+		if(value.size()==k) {
+			count++;
+		}
+		
+	//	System.out.println();
+		
+		for(int j=1;j<str.length()-k;j++) {
+			//if the value is greater than 1
+			
+			if(value.get(str.charAt(j-1))>1) {
+				value.put(str.charAt(j-1), value.getOrDefault(str.charAt(j-1), 0)-1);
+			} else {
+				value.remove(str.charAt(j-1));
+			}
+			
+			//if the value is not present
+			value.put(str.charAt(j+k-1), value.getOrDefault(str.charAt(j+k-1), 0)+1);
+			
+			}
+		if(value.size()==k) {
+			count++;
+		}
+		
+		System.out.println(count);
+}
+}
+
+
